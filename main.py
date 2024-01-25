3ee38709d87c4ace1e572429501f41f66ead77c5
dl-bk <BahtX_hn46@student.itstep.org>
2024-01-25 16:43:06 +0100

    ex 2

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine, Column, Integer, String, Sequence, Date
 from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy.sql import text
 import json
-
+from re import search, IGNORECASE
 # Зчитування конфігураційних даних з файлу
 with open('config.json') as f:
     config = json.load(f)
@@ -26,6 +26,14 @@ class Person(Base):
     country = Column(String(50))
     birth_date = Column(Date)
 
+def check_query(query):
+    if not search(r'\bpeople\b', query, IGNORECASE):
+        raise Exception(f"Incorrect table name. Name must be 'people'")
+    
+    if search(r'\bupdate\b', query, IGNORECASE) or search(r'\bdelete\b', query, IGNORECASE):
+        if not search(r'\bwhere\b', query, IGNORECASE):
+            raise Exception("Update or Delete query must contain 'Where' statement")
+
 #створення таблиці
 Base.metadata.create_all(engine)
 
@@ -33,19 +41,20 @@ Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
 session = Session()
 
-person1 = Person(first_name='John', last_name='Doe', city='New York', country='USA', birth_date='1990-01-15')
-person2 = Person(first_name='Jane', last_name='Smith', city='London', country='UK', birth_date='1985-03-22')
-session.add_all([person1, person2])
-session.commit()
+# person1 = Person(first_name='John', last_name='Doe', city='New York', country='USA', birth_date='1990-01-15')
+# person2 = Person(first_name='Jane', last_name='Smith', city='London', country='UK', birth_date='1985-03-22')
+# session.add_all([person1, person2])
+# session.commit()
+
+
 
 while True:
-    user_query = input("Enter Select query or 'exit' to exit: ")
+    user_query = input("Enter query or 'exit' to exit: ")
     if user_query.lower() == 'exit':
         break
 
     try:
-        if 'select' not in user_query.lower():
-            raise Exception("You can execute only 'select' queries")
+        check_query(user_query)
         result = session.execute(text(user_query))
         rows = result.fetchall()
         if rows:
